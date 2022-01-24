3cfc87c9c2d9bb0f550080f37045a884db77a68f
HFHG-DELTA <98241233+HFHG-DELTA@users.noreply.github.com>
2022-01-23 18:53:37 -0600

    Added Favors and Wizard College Rank colors
    Added Favors and Wizard College Rank colors for central column display

--- idleloops-predictor.user.js
+++ idleloops-predictor.user.js
@@ -388,6 +388,8 @@ const Koviko = {
       ul.koviko .ritual{color:#ff1493}
       ul.koviko .artifacts{color:#ffd700}
       ul.koviko .mind{color:#006400}
+      ul.koviko .favors{color:#6392fe}
+      ul.koviko .wizrank{color:#7303bb}
       .actionOptions .showthis {width:max-content;bottom:100%;max-width:400px;margin-bottom:5px;}
       .travelContainer, .actionContainer {position:relative;}
       \`;
